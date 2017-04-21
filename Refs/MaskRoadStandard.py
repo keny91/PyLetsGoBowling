e6ac0abf11fa18590024959444183e929f430bbd
Luis Herranz Mañas <luishmanas@gmail.com>
2017-04-21 22:58:02 +0100

    Added mask
    We are not interested in the full extracted image. We will be cutting out the ROI.

---
+++ Refs/MaskRoadStandard.py
@@ -0,0 +1,5 @@
+import numpy as np
+# import cv2
+
+ROAD_ROI_POLYGON_AS_array = np.array([[10, 500], [10, 300], [300, 200], [500, 200], [800, 300], [800, 500]])
+#ROAD_ROI_POLYGON_AS_Vec2i = np.array(cv2.Vec2i(10, 500))
--- Scripts/FrameExtraction.py
+++ Scripts/FrameExtraction.py
@@ -1,11 +1,12 @@
 from PIL import ImageGrab
 import numpy as np
+import cv2
 
 
 def getFrame():
     printScreen_pil = ImageGrab.grab()  # ImageGrab only works for windows. Might need an alternative
     # printScreen_numpy = np.array(printScreen_pil.getdata(), dtype='uint8') \
-        # .reshape((printScreen_pil.size[1], printScreen_pil.size[0], 3))
+    # .reshape((printScreen_pil.size[1], printScreen_pil.size[0], 3))
     return np.array(printScreen_pil)
 
 
@@ -14,3 +15,14 @@ def getFrameFromSquare(bbox=(0, 40, 800, 640)):
     # printScreen_numpy = np.array(printScreen_pil.getdata(), dtype='uint8') \
     #     .reshape((printScreen_pil.size[1], printScreen_pil.size[0], 3))
     return np.array(printScreen_pil)
+
+
+def MaskRoadROI(image, PolygonPoints):
+    # initialize the mask as all 0s as a same size image
+    mask = np.zeros_like(image)
+    # fill the shape with 1s (255)
+    cv2.fillPoly(mask, PolygonPoints, 255)
+    # apply mask to the image; do and AND bit operation:
+    # 1 & 1 = 1; 1 & 0 = 0; 0 & 0 = 0
+    masked = cv2.bitwise_and(image, mask)
+    return masked
