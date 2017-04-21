af89b9d37424d2d1bfb7baee7b2e9cd861b76858
Luis Herranz Mañas <luishmanas@gmail.com>
2017-04-21 13:44:52 +0100

    And forgot to stage changes again ...
    Dumb dumb dumb

--- Scripts/GameControl.py
+++ Scripts/GameControl.py
@@ -9,6 +9,7 @@ def WaitLoop(numLoops=4, waitTimeperLoop=1):
         time.sleep(waitTimeperLoop)
 
 
+## Press a key outside this environment -> NOT COMPATIBLE WITH THE DIRECTX inputs
 def SetKeyDown(theKey):
     print('Key is down: {}'.format(theKey))
     pyautogui.keyDown(theKey)
