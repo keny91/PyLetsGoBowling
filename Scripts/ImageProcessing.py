245d0ba37b3f7c72dc4f74e9d7549733c03520a0
Luis Herranz Mañas <luishmanas@gmail.com>
2017-04-22 22:30:18 +0100

    Find Hough lines
    Also alternative way to find the edges using a vertical Sobel filter.

--- Scripts/ImageProcessing.py
+++ Scripts/ImageProcessing.py
@@ -6,3 +6,23 @@ def ExtractCannyEdges(theImage, th1=200, th2=300):
     outputImage = cv2.cvtColor(theImage, cv2.COLOR_BGR2GRAY)
     outputImage = cv2.Canny(outputImage, th1, th2)
     return outputImage
+
+
+def SobelFilterX(theImage, thesize=5):
+    # sobelx = cv2.Sobel(theImage, cv2.CV_64F, 1, 0, ksize=thesize)
+    sobelx = cv2.Sobel(theImage, cv2.CV_8U, 1, 0, ksize=thesize)
+    # is this necessary?
+    abs_sobelx = np.absolute(sobelx)
+    outputImage = cv2.cvtColor(abs_sobelx, cv2.COLOR_BGR2GRAY)
+    return outputImage
+
+
+def DrawLines(image, lines):
+    try:
+        for line in lines:
+            coords = line[0]
+            cv2.line(image, (coords[0], coords[1]), (coords[2], coords[3]), [255, 255, 255], 3)
+
+    except:
+        pass
+
